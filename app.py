3e2c131449fb11a4fb2192fbb4dac7e38bab6a6a
sophXmoore1 <116595477+sophXmoore1@users.noreply.github.com>
2023-05-21 13:40:15 -0400

    update path

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     output = replicate.run("salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
-    input={"image": open("./waterBottle.jpg", "rb"),
+    input={"image": "../my-photo.png",
         "task": "visual_question_answering",
         "question": "What item is this?"})
     return output
